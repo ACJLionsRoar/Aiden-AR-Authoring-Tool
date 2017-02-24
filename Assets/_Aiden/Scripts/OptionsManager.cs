92adfd2ac234a5316fcf51f82c5f21a0e921ae49
Nihal369 <nihalismailk@gmail.com>
2017-02-24 22:49:13 +0530

    5 Countries added

---
+++ Assets/_Aiden/Scripts/OptionsManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionsManager : MonoBehaviour {
+
+	Slider mergeSlider,occlusionSlider,rotationSlider,zoomSlider,transferSlider;
+
+	// Use this for initialization
+	void Start () {
+		mergeSlider.value = 1;//To be changed
+		occlusionSlider.value = 1;//To be changed
+		rotationSlider.value = 1;//To be changed
+		zoomSlider.value = 1;//To be changed
+		transferSlider.value = 1;//To be changed
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
