c14967a2b8b00a10eb5fa736c2460a15d9f6b08e
Nihal369 <nihalismailk@gmail.com>
2017-03-28 00:55:38 +0530

    Sleepy,Gesture detector still buggy

--- Assets/_Aiden/Scripts/GestureDetector.cs
+++ Assets/_Aiden/Scripts/GestureDetector.cs
@@ -4,22 +4,17 @@ using System.Collections;
 
 public class GestureDetector : MonoBehaviour {
 
-	public GameObject test;
 	public Text testText;
+	static GameObject objectDetected;
 	bool isRotating;
 	Vector2 startVector;
 	float minDistanceBetweenFingers,minAngle;
 
-
 	float zoomSpeed=0.05f;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		if (Input.touchCount == 2 && PlayerPreferenceManager.getZoom () == 1) {//Check if total touches are 2
 			detectZoom();
 		}
@@ -50,33 +45,50 @@ public class GestureDetector : MonoBehaviour {
 
 		if (magnitudeDifference > 1) {
 			//Pinch In-Zoom Decrease
-			if (test.transform.localScale.x >= 0.05) {
-				test.transform.localScale -= new Vector3 (zoomSpeed, zoomSpeed, zoomSpeed);//Decrease Size
-			}
-		} else {
-			//Pinch Out-Zoom Increase
-			if (test.transform.localScale.x <= 3) {
-				test.transform.localScale += new Vector3 (zoomSpeed, zoomSpeed, zoomSpeed);//Increase size
+			if (objectDetected != null) {
+				if (objectDetected.transform.localScale.x >= 0.05) {
+					objectDetected.transform.localScale -= new Vector3 (zoomSpeed, zoomSpeed, zoomSpeed);//Decrease Size
+				} else {
+					//Pinch Out-Zoom Increase
+					if (objectDetected.transform.localScale.x <= 3) {
+						objectDetected.transform.localScale += new Vector3 (zoomSpeed, zoomSpeed, zoomSpeed);//Increase size
+					}
+				}
 			}
 		}
 	}
 
 	void detectRotation()
 	{
-		//Rotation gesture
-		if (!isRotating) {
-			Vector2 startVector = Input.GetTouch (1).position - Input.GetTouch (0).position;
-			isRotating = startVector.sqrMagnitude > minDistanceBetweenFingers * minDistanceBetweenFingers;
-		} else {
-			Vector2 currentVector = Input.GetTouch (1).position - Input.GetTouch (0).position;
+		if (objectDetected != null) {
+			//Rotation gesture
+			if (!isRotating) {
+				Vector2 startVector = Input.GetTouch (1).position - Input.GetTouch (0).position;
+				isRotating = startVector.sqrMagnitude > minDistanceBetweenFingers * minDistanceBetweenFingers;
+			} else {
+				Vector2 currentVector = Input.GetTouch (1).position - Input.GetTouch (0).position;
+			}
 		}
 	}
 
 	void detectTransfer()
 	{
-		//Transfer Gesture
-		Vector3 mousePosition = Input.mousePosition;
-		mousePosition.z = 10;//Distance from camera to screen,Camera at -10 and screen at 0
-		test.transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+		if(objectDetected!=null)
+		{
+			//Transfer Gesture
+			Vector3 mousePosition = Input.mousePosition;
+			mousePosition.z = 10;//Distance from camera to screen,Camera at -10 and screen at 0
+			objectDetected.transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+		}
+	}
+
+	public static void setObjectDetected(GameObject _objectDetected)
+	{
+		objectDetected = _objectDetected;
+	}
+
+	public static void setObjectDetectedNull()
+	{
+		objectDetected = null;
 	}
 }
